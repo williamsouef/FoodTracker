e507507843b182547abd1f05e4b40c9c9faf7bcf
William Souef <williamsoue@gmail.com>
2020-01-04 10:43:23 -0300

    Initial Commit

---
+++ FoodTrack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTrack
+//
+//  Created by William Souef on 04/01/2020.
+//  Copyright © 2020 William Souef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
